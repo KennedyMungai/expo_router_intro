dc97ecefeae9549a605d4e640950a8373715f9b1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-23 11:40:44 +0300

    Added some Tabs to the TabsLayout file

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router'
 
 const TabsLayout = () => {
 	return (
-		<View>
-			<Text>TabsLayout</Text>
-		</View>
+		<Tabs>
+			<Tabs.Screen name='index' />
+			<Tabs.Screen name='users/[id]' />
+		</Tabs>
 	)
 }
 
