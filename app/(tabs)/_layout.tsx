2f9d9c6e313125b393894ca208d9768ab9ea7dd7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-23 11:57:21 +0300

    Updated the names of the individual tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,8 +4,14 @@ import React from 'react'
 const TabsLayout = () => {
 	return (
 		<Tabs>
-			<Tabs.Screen name='index' options={{ headerTitle: 'Home' }} />
-			<Tabs.Screen name='users/[id]' options={{ headerTitle: 'User' }} />
+			<Tabs.Screen
+				name='index'
+				options={{ headerTitle: 'Home', title: 'Home' }}
+			/>
+			<Tabs.Screen
+				name='users/[id]'
+				options={{ headerTitle: 'User', title: 'User' }}
+			/>
 		</Tabs>
 	)
 }
