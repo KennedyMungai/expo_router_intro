2b9612b747fa9bfc153665f21798440f8c659556
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-23 11:46:38 +0300

    Added the option to not show the header of the application

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,8 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome'
 import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider
 } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
@@ -12,8 +12,8 @@ import { useEffect } from 'react'
 import { useColorScheme } from '@/components/useColorScheme'
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary
+	// Catch any errors thrown by the Layout component.
+	ErrorBoundary
 } from 'expo-router'
 
 export const unstable_settings = {
@@ -55,9 +55,9 @@ function RootLayoutNav() {
 		<ThemeProvider
 			value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
 		>
-      <Stack>
-        <Stack.Screen name='(tabs)' />
-      </Stack>
+			<Stack>
+				<Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+			</Stack>
 		</ThemeProvider>
 	)
 }
