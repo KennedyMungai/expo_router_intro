d8bc96997c3c55c1af6bb3f8fd7f86549b4782e6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-02-23 11:47:47 +0300

    Added some options to the tabbed screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,8 +4,8 @@ import { Tabs } from 'expo-router'
 const TabsLayout = () => {
 	return (
 		<Tabs>
-			<Tabs.Screen name='index' />
-			<Tabs.Screen name='users/[id]' />
+			<Tabs.Screen name='index' options={{ headerTitle: 'Home' }} />
+			<Tabs.Screen name='users/[id]' options={{ headerTitle: 'User' }} />
 		</Tabs>
 	)
 }
